ac0f06fa61bb88364a2b850763656ba0ce3e6c4f
João Oliveira <1130537@isep.ipp.pt>
2017-04-21 10:20:34 +0100

    Criação do model e da tabela sensores

---
+++ cpr-pt/database/migrations/2017_04_21_090933_create_sensors_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSensorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sensors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('info');
+            $table->string('units');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sensors');
+    }
+}
