2770139c192f7eb02e73727b4f36aaad044368cd
João Oliveira <1130537@isep.ipp.pt>
2017-07-03 15:46:11 +0100

    Front Page

--- cpr-pt/resources/views/welcome.blade.php
+++ cpr-pt/resources/views/welcome.blade.php
@@ -2,74 +2,50 @@
 <html >
 <head>
   <meta charset="UTF-8">
+   <link rel="icon" href="/heart_md.png">
   <title>CPR PT</title>
-      <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-      <link rel="stylesheet" href="/css/welcome.css">
 
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+
+  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:900'>
+
+      <link rel="stylesheet" href="css/welcome.css">
 </head>
 
 <body>
-  <html>
-  <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>jQuery Parallax Plugin Demo</title>
+  <ul class="Words">
+  <li class="Words-line">
+    <p>&nbsp;</p>
+     <a href="/home"><p>Cardio</p></a>
+  </li>
+  <li class="Words-line">
+    <a href="/home"><p>Cardio</p></a>
+    <a href="/home"><p>Pulmonary</p></a>
+  </li>
+  <li class="Words-line">
+    <a href="/home"><p>Pulmonary</p></a>
+    <a href="/home"><p>Reanimation</p></a>
+  </li>
+  <li class="Words-line">
+    <a href="/home"><p>Reanimation</p></a>
+    <a href="/home"><p>Personal</p></a>
+  </li>
+  <li class="Words-line">
+    <a href="/home"><p>Personal</p></a>
+     <a href="/home"><p>Trainer</p></a>
+  </li>
+  <li class="Words-line">
+     <a href="/home"><p>Trainer</p></a>
+    <a href="https://www.linkedin.com/in/jo%C3%A3o-oliveira-69819410b/"> <p>By</p></a>
+  </li>
+  <li class="Words-line">
+    <a href="https://www.linkedin.com/in/jo%C3%A3o-oliveira-69819410b/"> <p>By</p></a>
+    <a href="https://www.linkedin.com/in/jo%C3%A3o-oliveira-69819410b/"> <p>Joao Oliveira</p></a>
+  </li>
 
-<script type="text/javascript" src="https://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
-<script type="text/javascript" src="https://ianlunn.co.uk/plugins/jquery-parallax/scripts/jquery.parallax-1.1.3.js"></script>
-<script type="text/javascript" src="https://ianlunn.co.uk/plugins/jquery-parallax/scripts/jquery.localscroll-1.2.7-min.js"></script>
-<script type="text/javascript" src="https://ianlunn.co.uk/plugins/jquery-parallax/scripts/jquery.scrollTo-1.4.2-min.js"></script>
-</head>
+</ul>
+  <script src='https://code.jquery.com/jquery-2.2.4.min.js'></script>
 
-<body>
-	<ul id="nav">
-		<li><a href="#intro" title="Next Section"><img src="https://ianlunn.co.uk/plugins/jquery-parallax/images/dot.png" alt="Link" /></a></li>
-	    <li><a href="#second" title="Next Section"><img src="https://ianlunn.co.uk/plugins/jquery-parallax/images/dot.png" alt="Link" /></a></li>
-	    <li><a href="#third" title="Next Section"><img src="https://ianlunn.co.uk/plugins/jquery-parallax/images/dot.png" alt="Link" /></a></li>
-	</ul>
-   <div class="container-fluid">
-      <div class="row">
-         <div id="intro">
-           <div class="container">
-             <div class="col-md-10 col-md-offset-3">
-                <h2 class="h2_1">Cardiopulmonary Reanimation</h2>
-                <p class="h2_2 text-center">Personal Trainer</p>
-             </div>
-           </div>
-      	</div> <!--#intro-->
-      </div>
-
-
-      	<div id="second">
-      		<div class="story"><div class="bg"></div>
-      	    	<div class="float-right">
-      	            <h2>Multiple Backgrounds</h2>
-      	            <p>The multiple backgrounds applied to this section are moved in a similar way to the first section -- every time the user scrolls down the page by a pixel, the positions of the backgrounds are changed.</p>
-      	            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean nibh erat, sagittis sit amet congue at, aliquam eu libero. Integer molestie, turpis vel ultrices facilisis, nisi mauris sollicitudin mauris, volutpat elementum enim urna eget odio. Donec egestas aliquet facilisis. Nunc eu nunc eget neque ornare fringilla. Nam vel sodales lectus. Nulla in pellentesque eros. Donec ultricies, enim vitae varius cursus, risus mauris iaculis neque, euismod sollicitudin metus erat vitae sapien. Sed pulvinar.</p>
-      	        </div>
-      	    </div> <!--.story-->
-
-      	</div> <!--#second-->
-
-      	<div id="third">
-      		<div class="story">
-               <div class="panel panel-default">
-                 <div class="panel-heading">
-                   <h3 class="panel-title">Titulo</h3>
-                 </div>
-                 <div class="panel-body">
-                     Body
-                 </div>
-                 <div class="panel-footer">
-                     Footer
-                 </div>
-               </div>
-      	    </div> <!--.story-->
-      	</div> <!--#third-->
-</div><!--container-->
-</body>
-</html>
-
-    <script src="/js/welcome.js"></script>
 
 </body>
 </html>
